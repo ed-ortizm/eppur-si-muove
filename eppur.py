3d664541f31becca12449d96f8073dba3046e420
Edgar Ortiz <ed.ortizm@gmail.com>
2020-03-31 11:07:03 -0300

    image of the sky generated for the 250K brightest star in aitoff projection generated (galactic coordinates)

--- eppur.py
+++ eppur.py
@@ -16,16 +16,22 @@ data_file = "gaia_data.vot"
 data = Table.read(data_file)
 # Summary information about the table
 data.info
+# http://learn.astropy.org/rst-tutorials/Coordinates-Transform.html
 # create a SkyCoord() object from the right ascension and the declination.
-# coordinates: ICRS(RA,Dec), the default frame
+# coordinates: ICRS(Ra,Dec), the default frame
 ra = data['ra']
 dec = data['dec']
-coordinates = SkyCoord(ra,dec)
-
-
+coord = SkyCoord(ra,dec)
+gal_coor = coord.galactic
+# wrapping angle
+gal_coor.data.lon.wrap_angle = 180. * u.degree
 
 # https://matplotlib.org/gallery/subplots_axes_and_figures/geo_demo.html
-#plt.figure()
-#plt.subplot(111, projection="aitoff")
-#plt.title("Aitoff")
-#plt.grid(True)
+plt.figure()
+plt.subplot(111, projection="aitoff")
+plt.title("Aitoff")
+plt.grid(True)
+# s:The marker size in points**2
+# alpha: The alpha blending value, between 0 (transparent) and 1 (opaque).
+plt.scatter(gal_coor.data.lon.radian, gal_coor.data.lat.radian,marker='*', s=1., alpha=0.05)
+plt.show()
