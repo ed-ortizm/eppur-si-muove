28df94162d61d4036d7384ddca4e1a36e81c569f
Edgar Ortiz <ed.ortizm@gmail.com>
2020-04-02 14:21:32 -0300

    rad vel plotting done, number of bins set to 7, seemed OK

--- eppur.py
+++ eppur.py
@@ -5,7 +5,6 @@ import sys
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcol
-#from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.cm as cm
 # Importing from astropy
 # To load the data downloaded from Gaia
@@ -14,8 +13,8 @@ from astropy.table import Table
 from astropy import units as u
 # SkyCoord --> convert coordinates between different references systems
 from astropy.coordinates import SkyCoord
-# Aitoff projection ​ in !!galactic coordinates!!.
 
+# Aitoff projection ​ in !!galactic coordinates!!.
 data_file = "gaia_data.vot"
 data = Table.read(data_file)
 # Summary information about the table
@@ -41,6 +40,7 @@ data['Gal latitude'] = gal_coor.b
 # alpha: The alpha blending value, between 0 (transparent) and 1 (opaque).
 #plt.scatter(gal_coor.data.lon.radian, gal_coor.data.lat.radian,marker='*', s=1., alpha=0.05)
 #plt.show()
+#plt.savefig("250K_brigtest_gaia_sources.pdf")
 
 ## Radial velocity: it is already computed respect to the sun
 # https://gea.esac.esa.int/archive/documentation/GDR2/Gaia_archive/chap_datamodel/sec_dm_main_tables/ssec_dm_gaia_source.html
@@ -57,50 +57,30 @@ rad_vel = data['radial_velocity']
 # non missing values I turn to np.logicalnot()
 mask = np.logical_not(rad_vel.mask)
 data_masked = data[mask][:5000]
+# I'll need the min and max radial velocity to create the color bar
 min_vel = np.min(data_masked['radial_velocity'])
 max_vel = np.max(data_masked['radial_velocity'])
-# masks for blue and red shifted stars (it is binary but well)
-blue_shifted_mask = data_masked['radial_velocity'] < 0
-blue_shifted = data_masked[blue_shifted_mask]
-red_shifted_mask  = data_masked['radial_velocity'] > 0
-red_shifted = data_masked[red_shifted_mask]
-# https://matplotlib.org/gallery/subplots_axes_and_figures/geo_demo.html
-plt.figure()
-plt.subplot(111, projection="aitoff")
-plt.title("Aitoff: radial velocity")
-plt.grid(True)
-# s:The marker size in points**2
-# alpha: The alpha blending value, between 0 (transparent) and 1 (opaque).
-plt.scatter(blue_shifted['Gal longitude'],blue_shifted['Gal latitude'] ,\
-marker='*', s=1., alpha=1,c='blue')
-plt.scatter(red_shifted['Gal longitude'],red_shifted['Gal latitude'] ,\
-marker='*', s=1., alpha=1,c='red')
-plt.show()
-# Now let's do it with a color bar. Actually I found something really cool
+
+## Now let's plot the radial velocities it with a color bar.
+# Actually I found something really cool
 # https://matplotlib.org/3.1.1/gallery/color/custom_cmap.html
+# https://stackoverflow.com/questions/25748183/python-making-color-bar-that-runs-from-red-to-blue
+
+# Here I define my color map
 colors = [(0, 0, 1), (1, 0, 0)]  # B -> R
-n_bin = 2  # Discretizes the interpolation into bins
-cmap_name = 'my_list'
-#fig, ax = plt.subplots(1, 1, projection="aitoff"
-#fig.subplots_adjust(left=0.02, bottom=0.06, right=0.95, top=0.94, wspace=0.05)
-# Create the colormap
-my_cm = mcol.LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bin)
+n_bin = 7  # Discretizes the interpolation into bins
 # Fewer bins will result in "coarser" colomap interpolation
+cmap_name = 'my_list'
+my_cm = mcol.LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bin)
 normalize = mcol.Normalize(vmin=min_vel,vmax=max_vel)
 mappable = cm.ScalarMappable(norm=normalize, cmap = my_cm)
 mappable.set_array([])
-#im = ax.imshow(Z, interpolation='nearest', origin='lower', cmap=cm)
-#ax.set_title("N bins: %s" % n_bin)
-#fig.colorbar(im, ax=ax)
-#mappable = cm.ScalarMappable(cmap = cm)
-#mappable.set_array([])
-
+# Now plotting
 plt.figure()
 plt.subplot(111, projection="aitoff")
-plt.title("Aitoff: radial velocity")
+plt.title("Aitoff")
 plt.scatter(data_masked['Gal longitude'],data_masked['Gal latitude'] ,\
-marker='*', s=1., alpha=1,c=mappable.to_rgba(data_masked['radial_velocity']))
-plt.colorbar(mappable)
-# The Sun has an apparent magnitude of −27
-# https://matplotlib.org/3.1.0/tutorials/colors/colorbar_only.html
+marker='*', s=6., alpha=1,c=mappable.to_rgba(data_masked['radial_velocity']))
+plt.colorbar(mappable, label="radial velocity")
 plt.show()
+plt.savefig("radial_velocity_" + str(n_bin) + ".pdf")
