d5e4e3f6f8e2deaf93e98e068692c0eab03c9b10
Edgar Ortiz <ed.ortizm@gmail.com>
2020-04-02 14:29:21 -0300

    I wasn't saving the figures, now I am. I implemented a for to get more bins

--- eppur.py
+++ eppur.py
@@ -71,16 +71,17 @@ colors = [(0, 0, 1), (1, 0, 0)]  # B -> R
 n_bin = 7  # Discretizes the interpolation into bins
 # Fewer bins will result in "coarser" colomap interpolation
 cmap_name = 'my_list'
-my_cm = mcol.LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bin)
-normalize = mcol.Normalize(vmin=min_vel,vmax=max_vel)
-mappable = cm.ScalarMappable(norm=normalize, cmap = my_cm)
-mappable.set_array([])
-# Now plotting
-plt.figure()
-plt.subplot(111, projection="aitoff")
-plt.title("Aitoff")
-plt.scatter(data_masked['Gal longitude'],data_masked['Gal latitude'] ,\
-marker='*', s=6., alpha=1,c=mappable.to_rgba(data_masked['radial_velocity']))
-plt.colorbar(mappable, label="radial velocity")
-plt.show()
-plt.savefig("radial_velocity_" + str(n_bin) + ".pdf")
+for n_bin in range(2,11):
+    my_cm = mcol.LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bin)
+    normalize = mcol.Normalize(vmin=min_vel,vmax=max_vel)
+    mappable = cm.ScalarMappable(norm=normalize, cmap = my_cm)
+    mappable.set_array([])
+    # Now plotting
+    plt.figure()
+    plt.subplot(111, projection="aitoff")
+    plt.title("Aitoff")
+    plt.scatter(data_masked['Gal longitude'],data_masked['Gal latitude'] ,\
+    marker='*', s=6., alpha=1,c=mappable.to_rgba(data_masked['radial_velocity']))
+    plt.colorbar(mappable, label="radial velocity")
+    plt.savefig("radial_velocity_" + str(n_bin) + ".pdf")
+    #plt.show()
